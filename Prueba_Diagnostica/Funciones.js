b2158adca7eedfe4429882735b285435de123298
Pedro Medina <134984408+pmedina-2019111@users.noreply.github.com>
2024-01-18 11:57:54 -0600

    subidinho parte 2

---
+++ Prueba_Diagnostica/Funciones.js
@@ -0,0 +1,4 @@
+//Initial References 
+const letra = document.getElementById("ContenedorLetra");
+const opciones = document.getElementById("ContenedorOpciones");
+const userInputSelection = document.getElementById("user-input-sectiom");

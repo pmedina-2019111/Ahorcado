7be80d9c9de470ff0f4bc0ef6a4871787b979245
Informatica <informatica@alumno.local>
2024-01-19 08:31:21 -0600

    Elevado Portafolio

--- Prueba_Diagnostica/Funciones.js
+++
@@ -1,4 +0,0 @@
-//Initial References 
-const letra = document.getElementById("ContenedorLetra");
-const opciones = document.getElementById("ContenedorOpciones");
-const userInputSelection = document.getElementById("user-input-sectiom");
